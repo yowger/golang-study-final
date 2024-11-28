d6389c6148069551e599efcb130ce4f1712b10bf
Roger Pantil <rogerpantilyowger@gmail.com>
2024-11-28 20:51:51 +0800

    update mongo connection

--- context-1/context.go
+++ context-1/context.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
+// controlling timeouts
+// cancelling go routines
+//  and passing metadata across your Go applications
+
+func main() {
+	ctx := context.Background()
+	exampleTimeout(ctx)
 }
+
+func exampleTimeout(ctx context.Context) {
+
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, 4*time.Second)
+	defer cancel()
+
+	done := make(chan struct{})
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		fmt.Println("Called the API")
+	case <-ctxWithTimeout.Done():
+		fmt.Println("on no timed out expired", ctxWithTimeout.Err())
+		// do some logic to handle the timeout
+		// logging, setting response (try again)
+	}
+}
+
+// ctx := context.Background()
+// ctxWithTimeout, cancel := context.WithTimeout(context.Background(), 4*time.Second)
--- mongo-1/main.go
+++ mongo-1/main.go
@@ -1,23 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
 	"myapp/config"
-	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
 )
+
+func connectMongoDB(uri string) (*mongo.Client, error) {
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	client, err := mongo.Connect(opts)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
 
 func main() {
 	cfg := config.LoadConfig()
 
-	server := &http.Server{
-		Addr:    ":" + cfg.AppPort,
-		Handler: nil,
+	client, err := connectMongoDB(cfg.MongoURI)
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB:", err)
 	}
+	defer func() {
+		if err := client.Disconnect(context.Background()); err != nil {
+			log.Fatal("Error disconnecting MongoDB:", err)
+		}
+	}()
 
-	fmt.Println("Starting server on port", cfg.AppPort)
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatal("failed to start server: ", err)
-	}
+	// server := &http.Server{
+	// 	Addr:    ":" + cfg.AppPort,
+	// 	Handler: nil,
+	// }
+
+	// fmt.Println("Starting server on port", cfg.AppPort)
+	// if err := server.ListenAndServe(); err != nil {
+	// 	log.Fatal("failed to start server: ", err)
+	// }
 
 }
