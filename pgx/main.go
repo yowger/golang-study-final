f68769ebc11e836b54533d83dfa5f8792a5b748f
Roger Pantil <rogerpantilyowger@gmail.com>
2024-12-02 21:32:29 +0800

    added create handler in pgx

--- pgx/main.go
+++ pgx/main.go
@@ -11,6 +11,12 @@ import (
 
 var conn *pgx.Conn
 
+type User struct {
+	ID    int     `json:"id"`
+	Name  string  `json:"name"`
+	Value float64 `json:"email"`
+}
+
 func main() {
 	var err error
 	if err := godotenv.Load(); err != nil {
@@ -25,14 +31,60 @@ func main() {
 	}
 	defer conn.Close(context.Background())
 
-	var name string
-	var value float64
+	newUser := User{Name: "John Doe", Value: 1.0}
+	userID, err := createUser(context.Background(), newUser)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "createUser: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Created user with ID: ", userID)
 
+	users, err := getAllUsers(context.Background())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "getAllUsers: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, user := range users {
+		fmt.Println("User: ", user)
+	}
+
+}
+
+func createUser(ctx context.Context, user User) (int, error) {
+	var id int
+	err := conn.QueryRow(ctx, "INSERT INTO playing_with_neon (name, value) VALUES ($1,$2) RETURNING id", user.Name, user.Value).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func getAllUsers(ctx context.Context) ([]User, error) {
+	rows, err := conn.Query(ctx, "SELECT id, name, value from playing_with_neon")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []User
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.ID, &user.Name, &user.Value)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+/*
 	conn.QueryRow(context.Background(), "Select name, value from playing_with_neon where id=$1", 2).Scan(&name, &value)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 		os.Exit(1)
 	}
-
-	fmt.Println(name, value)
-}
+*/
